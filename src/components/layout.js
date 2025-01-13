57b04d9db629c523d0d39b717b007bced5e03663
whr2206 <whr2206@hotmail.com>
2025-01-12 22:58:33 -0500

    added icons to navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,16 +3,20 @@ import { Link } from 'gatsby'
 import {
   container,
   heading,
+  navBar,
   navLinks,
   navLinkItem,
   navLinkText
 } from './layout.module.css'
 
+import { FaGithub } from 'react-icons/fa';
+import { CgDarkMode } from "react-icons/cg";
+
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       
-      <nav>
+      <nav className={navBar}>
         <ul className={navLinks}>
         <li className={navLinkItem}>
             <Link to="/" className={navLinkText}>
@@ -32,6 +36,16 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
               About Me
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="#" className={navLinkText}>
+              <CgDarkMode size={24} />
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="https://github.com/whr2206" target="_blank" className={navLinkText}>
+              <FaGithub size={24} />
             </Link>
           </li>
         </ul>
