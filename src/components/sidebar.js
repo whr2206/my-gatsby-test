9c7655e0f19858b9863684f3e609912ced3682bb
whr2206 <whr2206@hotmail.com>
2025-01-13 23:11:58 -0500

    Created sidebar

---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import styles from './sidebar.module.css'
+
+  const Sidebar = () => {
+    return (
+      <div>
+        <p>Stuff here.</p>
+      </div>
+    );
+  };
+  
+// Step 3: Export your component
+export default Sidebar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 // Step 1: Import React
 import * as React from 'react'
 import Navbar from '../components/navbar'
+import Sidebar from '../components/sidebar'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-      <Navbar />
+      <><Navbar /><Sidebar /></>
   )
 }
 
