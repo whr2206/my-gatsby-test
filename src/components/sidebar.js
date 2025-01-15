963a731b41e7900c5146c5bf1ff385742ef9b30e
whr2206 <whr2206@hotmail.com>
2025-01-14 23:00:03 -0500

    fixed css class variable issue

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,16 +1,17 @@
 import * as React from 'react'
 import {
-  sideNav
+  sideNav,
+  sideHeading
 } from './sidebar.module.css'
 
   const Sidebar = () => {
     return (
       <div className={sideNav}>
-        <h4>About Me</h4>
+        <h4 className={sideHeading}>About Me</h4>
           <p>Culpa labore sunt deserunt consectetur. Esse quis laborum tempor ea fugiat occaecat mollit id aliquip sunt. Ipsum nisi ullamco cupidatat minim dolore id nulla qui veniam consectetur nisi.</p>
-        <h4>Stay in Touch</h4>
+        <h4 className={sideHeading}>Stay in Touch</h4>
           <p>Know when I make something new!</p>
-        <h4>My Favorite Posts</h4>
+        <h4 className={sideHeading}>My Favorite Posts</h4>
       </div>
     );
   };
